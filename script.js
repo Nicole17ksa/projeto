e516644b5bd9949c9329fed0d4d31903ff93f8cf
Nicole Silva <nicolesouza2440@gmail.com>
2025-05-20 17:10:02 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    // substituir a img principal 
+    if (html.classList.contains('light')) {
+        // se tiver light mode, add a img light
+        img.setAttribute('src','.avatar.png')
+    } else {
+        // se tiver sem light mode, manter a img normal
+        img.setAttribute('src', 'avatar-light.png')
+    }
+}
