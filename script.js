afaede069115f8e58b1c8c0a98364f5878a75610
Nicole Silva <nicolesouza2440@gmail.com>
2025-05-20 21:38:34 -0300

    alterei a cor

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 function toggleMode() {
     const html = document.documentElement
-    html.classList.toggle('light')
+    html.classList.toggle("light")
 
     //pegar a tag img
     const img = document.querySelector("#profile img")
 
     // substituir a img principal 
-    if (html.classList.contains('light')) {
+    if (html.classList.contains("light")) {
         // se tiver light mode, add a img light
-        img.setAttribute('src','.avatar.png')
+        img.setAttribute("src","avatar-light.png")
     } else {
         // se tiver sem light mode, manter a img normal
-        img.setAttribute('src', 'avatar-light.png')
-    }
+        img.setAttribute("src", "avatar.png")
+    } 
 }
